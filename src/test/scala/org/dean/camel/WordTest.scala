5e67455da1c9361d63ca577936da920f9fd0d99d
duangduangda <dongyhemail@sina.com>
2019-03-03 18:21:19 +0800

    first commit

---
+++ src/test/scala/org/dean/camel/WordTest.scala
@@ -0,0 +1,17 @@
+package org.dean.camel
+
+import org.junit._
+import Assert._
+
+@Test
+class WordCountTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
